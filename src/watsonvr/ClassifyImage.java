072a696e65307b26e7f60f4a7ccfde99afc139bc
Nico Wickersheim <nico.wickersheim@yahoo.de>
2020-03-09 22:38:00 +0100

    formatting

--- src/watsonvr/ClassifyImage.java
+++ src/watsonvr/ClassifyImage.java
@@ -9,13 +9,18 @@ public class ClassifyImage {
 
 	public static void main(String[] args) {
 		
-		Keys key = new Keys();		
+		// Create Object for API settings
+		Keys key = new Keys();
+		
+		// Connecting to Watson Server
 		IamAuthenticator authenticator = new IamAuthenticator(key.apiKey);
 		VisualRecognition visualRecognition = new VisualRecognition("2018-03-15", authenticator);
 		visualRecognition.setServiceUrl(key.serviceUrl);
-
+		
+		// Set image for AI analyze
 		String imageUrl = "https://www.trendswm.de/wp-content/uploads/2014/10/zulieferer-klein1.jpg";
 
+		// Execute vr
 		ClassifyOptions classifyOptions = new ClassifyOptions.Builder().url(imageUrl).build();
 		ClassifiedImages result = visualRecognition.classify(classifyOptions).execute().getResult();
 		System.out.println("\n******** Classify with the General model ********\n" + result
